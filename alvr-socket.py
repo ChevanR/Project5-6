6272ebcb3a88506d4bbb5d6d9c820c01c3b05355
Dragy <>
2024-11-12 13:00:56 +0100

    Fixed the extra [ characters

--- alvr-socket.py
+++ alvr-socket.py
@@ -14,7 +14,8 @@ LOG_DIR = "alvr-logs"
 
 # Ensure the log directory exists
 os.makedirs(LOG_DIR, exist_ok=True)
-RECEIVE_TIMEOUT = 60  # Time to wait for a message before timing out (in seconds)
+# Time to wait for a message before timing out (in seconds)
+RECEIVE_TIMEOUT = 60
 
 # Generate a unique filename based on the current date and time
 filename = datetime.now().strftime("%Y%m%d_%H%M%S_%f") + ".json"
@@ -36,32 +37,31 @@ def close_json_array():
         # Append the closing bracket
         f.write(b']')
 
+
 # Register the function to run when the program exits
 atexit.register(close_json_array)
 
-async def hello():
-    while True:
-        try:
-            async with connect("ws://localhost:8082/api/events",ping_interval=5, ping_timeout=20) as websocket:
-            # Write the JSON data to a new file
-                with open(LOG_FILE, 'a') as f:
-                    f.write('[')
 
+async def hello():
+    with open(LOG_FILE, 'a') as f:
+        f.write('[')
+        while True:
+            try:
+                async with connect("ws://localhost:8082/api/events", ping_interval=5, ping_timeout=20) as websocket:
+                    # Write the JSON data to a new file
                     while True:
                         try:
-                    # Receive a message from the WebSocket
+                            # Receive a message from the WebSocket
                             message = await asyncio.wait_for(websocket.recv(), timeout=RECEIVE_TIMEOUT)
-                    # Parse the message as JSON
+                            # Parse the message as JSON
                             data = json.loads(message)
                             json.dump(data, f, indent=3)
                             f.write(',')
                         except json.JSONDecodeError:
                             print("Received invalid JSON message")
                             continue
-        except Exception:
-            continue
-
-               
+            except Exception:
+                continue
 
 
 if __name__ == "__main__":
